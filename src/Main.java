2bc6eeefd7e7b438d303e1e938aea5904ed6b838
AnasElgarhy <44965145+Anas-Elgarhy@users.noreply.github.com>
2021-07-08 21:40:04 +0200

    Code improvement and bug fixes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -9,14 +10,21 @@ public class Main {
         int currentYear = Integer.parseInt(df.format(currentDate));
         final int numOfDaysInYear = 356;
         String firstName;
-        int birthYear;
+        int birthYear = currentYear + 1;
+        int years;
         System.out.print("Enter Your First Name: ");
         firstName = input.next(); // read one token from user
-        do
-            System.out.print("Enter Year of Birth: ");
-        while ((birthYear = input.nextInt()) >= currentYear);
+        while(birthYear >= currentYear) {
+            try{
+                System.out.print("Enter Year of Birth: ");
+                birthYear = input.nextInt();
+            } catch(InputMismatchException ex){
+                System.out.println("The entered value is not an integer!");
+                System.exit(1);
+            }
+        }
         // calc num of years
-        int years = currentYear-birthYear;
+        years = currentYear-birthYear;
         // Print result
         System.out.printf("Hello %s! You have been alive for :\n", firstName);
         System.out.println("Years\tMonth\t Days");
